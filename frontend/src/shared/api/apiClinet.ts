c7e0a86b8dcb0bb35f26f142d0f7c33dbc15acec
povidllo <kuzminovm78@gmail.com>
2026-05-08 15:01:51 +0700

    fix(frontend): Удаление JWT после 401

--- frontend/src/shared/api/apiClinet.ts
+++ frontend/src/shared/api/apiClinet.ts
@@ -2,11 +2,39 @@ import { AuthApi, Configuration, ResultsApi, TextsApi } from "./generated";
 
 const SERVER_PORT = 3000;
 
-const config: Configuration = new Configuration({
-  basePath: `http://localhost:${SERVER_PORT}`,
-  accessToken: () => localStorage.getItem("token") ?? "",
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: `http://localhost:${SERVER_PORT}`,
 });
 
-export const textsApi: TextsApi = new TextsApi(config);
-export const authApi: AuthApi = new AuthApi(config);
-export const resultsApi: ResultsApi = new ResultsApi(config);
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+const config = new Configuration({
+  basePath: `http://localhost:${SERVER_PORT}`,
+});
+
+export const textsApi = new TextsApi(config, undefined, axiosInstance);
+
+export const authApi = new AuthApi(config, undefined, axiosInstance);
+
+export const resultsApi = new ResultsApi(config, undefined, axiosInstance);
