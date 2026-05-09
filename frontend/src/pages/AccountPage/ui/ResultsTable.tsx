3f408ac03c34b9dfdeae4e6a8f5c20ea69b0fdc7
povidllo <kuzminovm78@gmail.com>
2026-05-09 22:38:32 +0700

    feat(frontend): Мемоизация для Results Table

--- frontend/src/pages/AccountPage/ui/ResultsTable.tsx
+++ frontend/src/pages/AccountPage/ui/ResultsTable.tsx
@@ -1,54 +1,68 @@
 import type { UserResults } from "@/entity/Results";
 import clsx from "clsx";
+import { memo } from "react";
 
 interface ResultsTableProps {
   results: UserResults[];
 }
 
-export const ResultsTable = ({ results }: ResultsTableProps) => {
-  return (
-    <div className="overflow-x-auto rounded-xl">
-      <table className="w-full border-collapse">
-        <thead className="bg-(--bg-color)">
-          <tr>
-            <th className="px-4 py-3 text-left text-(--sub-color)">cpm</th>
+export const ResultsTable = memo(
+  ({ results }: ResultsTableProps) => {
+    return (
+      <div className="overflow-x-auto rounded-xl">
+        <table className="w-full border-collapse">
+          <thead className="bg-(--bg-color)">
+            <tr>
+              <th className="px-4 py-3 text-left text-(--sub-color)">cpm</th>
 
-            <th className="px-4 py-3 text-left text-(--sub-color)">wpm</th>
+              <th className="px-4 py-3 text-left text-(--sub-color)">wpm</th>
 
-            <th className="px-4 py-3 text-left text-(--sub-color)">accuracy</th>
+              <th className="px-4 py-3 text-left text-(--sub-color)">
+                accuracy
+              </th>
 
-            <th className="px-4 py-3 text-left text-(--sub-color)">language</th>
+              <th className="px-4 py-3 text-left text-(--sub-color)">
+                language
+              </th>
 
-            <th className="px-4 py-3 text-left text-(--sub-color)">type</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {results.map((el, index) => (
-            <tr
-              key={el.resultId}
-              className={clsx(
-                "transition-colors",
-                index % 2 === 0 && "bg-(--color-sub-alt)",
-              )}
-            >
-              <td className="px-4 py-3 text-(--text-color)">{el.cpm}</td>
-
-              <td className="px-4 py-3 text-(--text-color)">{el.wpm}</td>
-
-              <td className="px-4 py-3 text-(--text-color)">{el.accuracy}%</td>
-
-              <td className="px-4 py-3 text-(--text-color)">
-                {el.text.language}
-              </td>
-
-              <td className="px-4 py-3 text-(--text-color)">
-                {el.text.lengthType}
-              </td>
+              <th className="px-4 py-3 text-left text-(--sub-color)">type</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+          </thead>
+
+          <tbody>
+            {results.map((el, index) => (
+              <tr
+                key={el.resultId}
+                className={clsx(
+                  "transition-colors",
+                  index % 2 === 0 && "bg-(--color-sub-alt)",
+                )}
+              >
+                <td className="px-4 py-3 text-(--text-color)">{el.cpm}</td>
+
+                <td className="px-4 py-3 text-(--text-color)">{el.wpm}</td>
+
+                <td className="px-4 py-3 text-(--text-color)">
+                  {el.accuracy}%
+                </td>
+
+                <td className="px-4 py-3 text-(--text-color)">
+                  {el.text.language}
+                </td>
+
+                <td className="px-4 py-3 text-(--text-color)">
+                  {el.text.lengthType}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.results.length === nextProps.results.length;
+  },
+);
+
+ResultsTable.displayName = "ResultsTable";
